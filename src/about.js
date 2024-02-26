f186a980427a43a5343f60d7c2357501a3f45caa
fbragee <145098995+fbragee@users.noreply.github.com>
2024-02-26 11:16:17 +0000

    html and home.js done

---
+++ src/about.js
@@ -0,0 +1,8 @@
+function about() {
+	const aboutH2 = document.createElement('h2');
+	const aboutP = document.createElement('p');
+
+	aboutH2.innerText = 'Menu';
+	aboutP.innerText =
+		'Best restaurant in the block, fresh caught fish served daily in delicious plates combined with local veggies, in order to create a light and healthy meal.';
+}
