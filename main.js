31a6fb453ba5e7fffae13472732d76f807878442
fbragee <145098995+fbragee@users.noreply.github.com>
2024-02-27 12:06:15 +0000

    Finsihed JS logic

--- main.js
+++ main.js
@@ -10,6 +10,16 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./src/about.js":
+/*!**********************!*\
+  !*** ./src/about.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (createAboutPage);\n\nfunction createAboutPage() {\n\tconst content = document.querySelector(\"#content\");\n\tconst pageContent = document.createElement(\"div\");\n\tpageContent.classList.add(\"page-content\");\n\n\tconst aboutH2 = document.createElement(\"h2\");\n\taboutH2.classList.add(\"about-title\");\n\taboutH2.innerText = \"About Us\";\n\n\tconst aboutP = document.createElement(\"p\");\n\taboutP.classList.add(\"about-paragraph\");\n\taboutP.innerText = \"Best restaurant in the block, fresh caught fish served daily in delicious plates combined with local veggies, in order to create a light and healthy meal.\";\n\n\tpageContent.appendChild(aboutH2);\n\tpageContent.appendChild(aboutP);\n\tcontent.appendChild(pageContent);\n}\n\n\n//# sourceURL=webpack://restaurant-page/./src/about.js?");
+
+/***/ }),
+
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
@@ -20,13 +30,23 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _pag
 
 /***/ }),
 
+/***/ "./src/menu.js":
+/*!*********************!*\
+  !*** ./src/menu.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (createMenuPage);\n\nfunction createMenuPage() {\n\tconst content = document.querySelector(\"#content\");\n\tconst pageContent = document.createElement(\"div\");\n\tpageContent.classList.add(\"page-content\");\n\n\tconst heading = document.createElement(\"h1\");\n\tconst menuList = document.createElement(\"ul\");\n\n\tconst menuItem1 = createLi();\n\tconst menuItem2 = createLi();\n\tconst menuItem3 = createLi();\n\n\theading.innerText = \"Menu\";\n\tmenuItem1.innerText = \"Bacalhau a bras\";\n\tmenuItem2.innerText = \"King crab salad\";\n\tmenuItem3.innerText = \"Bitoque de atum\";\n\n\tmenuList.appendChild(menuItem1);\n\tmenuList.appendChild(menuItem2);\n\tmenuList.appendChild(menuItem3);\n\n\tpageContent.appendChild(heading);\n\tpageContent.appendChild(menuList);\n\tcontent.appendChild(pageContent);\n}\n\nfunction createLi() {\n\treturn document.createElement(\"li\");\n}\n\n\n//# sourceURL=webpack://restaurant-page/./src/menu.js?");
+
+/***/ }),
+
 /***/ "./src/pageLoad.js":
 /*!*************************!*\
   !*** ./src/pageLoad.js ***!
   \*************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _restaurant__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./restaurant */ \"./src/restaurant.js\");\n\n\nfunction initalLoad() {\n\t(0,_restaurant__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (initalLoad);\n\n\n//# sourceURL=webpack://restaurant-page/./src/pageLoad.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _restaurant__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./restaurant */ \"./src/restaurant.js\");\n/* harmony import */ var _tabs__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./tabs */ \"./src/tabs.js\");\n/* harmony import */ var _menu__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./menu */ \"./src/menu.js\");\n\n\n\n\nfunction initalLoad() {\n\t(0,_tabs__WEBPACK_IMPORTED_MODULE_1__[\"default\"])();\n\t(0,_restaurant__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\n\t(0,_menu__WEBPACK_IMPORTED_MODULE_2__[\"default\"])();\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (initalLoad);\n\n\n//# sourceURL=webpack://restaurant-page/./src/pageLoad.js?");
 
 /***/ }),
 
@@ -37,6 +57,16 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst createRestaurantHomePage = () => {\n\t//Content div\n\tconst content = document.querySelector(\"#content\");\n\tconst pageContent = document.createElement(\"div\");\n\tpageContent.classList.add(\"page-content\");\n\n\t//Image\n\tconst image = document.createElement(\"img\");\n\timage.src = \"https://vistapointe.net/images/restaurant-3.jpg\";\n\timage.width = \"300\";\n\tpageContent.appendChild(image);\n\n\t//Headline Element\n\tconst headline = document.createElement(\"h1\");\n\theadline.textContent = \"Sea Breeze\";\n\tpageContent.appendChild(headline);\n\n\t//Paragraph Element\n\tconst subText = document.createElement(\"p\");\n\tsubText.textContent = \"Traditional Portuguese seafood restaurant, in an upscale venue\";\n\tpageContent.appendChild(subText);\n\tcontent.appendChild(pageContent);\n};\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (createRestaurantHomePage);\n\n\n//# sourceURL=webpack://restaurant-page/./src/restaurant.js?");
+
+/***/ }),
+
+/***/ "./src/tabs.js":
+/*!*********************!*\
+  !*** ./src/tabs.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _about__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./about */ \"./src/about.js\");\n/* harmony import */ var _menu__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./menu */ \"./src/menu.js\");\n/* harmony import */ var _restaurant__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./restaurant */ \"./src/restaurant.js\");\n\n\n\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (createTabs);\n\nfunction createTabs() {\n\tconst content = document.querySelector(\"#content\");\n\n\tconst div1 = document.createElement(\"div\");\n\tconst div2 = document.createElement(\"div\");\n\tconst div3 = document.createElement(\"div\");\n\n\tdiv1.setAttribute(\"id\", \"home-btn\");\n\tdiv2.setAttribute(\"id\", \"menu-btn\");\n\tdiv3.setAttribute(\"id\", \"about-btn\");\n\n\tdiv1.classList.add(\"tab\");\n\tdiv2.classList.add(\"tab\");\n\tdiv3.classList.add(\"tab\");\n\n\tdiv1.textContent = \"Home\";\n\tdiv2.textContent = \"Menu\";\n\tdiv3.textContent = \"About\";\n\n\tcontent.appendChild(div1);\n\tcontent.appendChild(div2);\n\tcontent.appendChild(div3);\n\n\tdiv1.addEventListener(\"click\", () => {\n\t\tclearContent();\n\t\t(0,_restaurant__WEBPACK_IMPORTED_MODULE_2__[\"default\"])();\n\t});\n\n\tdiv2.addEventListener(\"click\", () => {\n\t\tclearContent();\n\t\t(0,_menu__WEBPACK_IMPORTED_MODULE_1__[\"default\"])();\n\t});\n\n\tdiv3.addEventListener(\"click\", () => {\n\t\tclearContent();\n\t\t(0,_about__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\n\t});\n}\n\nfunction clearContent() {\n\tconst content = document.querySelector(\"#content\");\n\tconst pageContent = document.querySelector(\".page-content\");\n\tif (pageContent) {\n\t\tcontent.removeChild(pageContent);\n\t}\n}\n\n\n//# sourceURL=webpack://restaurant-page/./src/tabs.js?");
 
 /***/ })
 
